6eeb9b6ffd6727ac2a9adf9a360a52ec35c07743
Hugetus <hugo.pulkkinen@icloud.com>
2025-01-21 15:57:07 +0200

    w?

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,6 +1,5 @@
 
 def debug_print(debug_msg=None, **kwargs):
-
     if debug_msg:
         print(debug_msg)
 
@@ -44,9 +43,11 @@ def merge(left, right):
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
+    
+    
+    value_list = [int(x.strip()) for x in input_str.split(',')]
 
-    quit(1)
-
+   
     debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
